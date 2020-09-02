8ed907bc67cc7440f9e326b98ef76b28e72a321f
Daniil Solyanoy <39529339+SP1K33@users.noreply.github.com>
2020-09-02 14:26:31 +0300

    Input processing moved to methods

--- MD5 Implementation/Program.cs
+++ MD5 Implementation/Program.cs
@@ -1,30 +1,45 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 
 namespace MD5_Implementation
 {
 	public static class Program
 	{
+		private static readonly MD5 _md5 = new MD5();
+
+		private static void ProcessFiles(string[] paths)
+		{
+			var validFiles = Utils.GetValidFiles(paths);
+			var fileBytes = Utils.GetFilesBytes(validFiles);
+			var results = _md5.Calculate(fileBytes);
+			for (var i = 0; i < results.Length; i++)
+			{
+				Console.WriteLine($"File -> {Path.GetFileName(validFiles[i])}\nMD5 Hash -> {results[i]}");
+			}
+			Console.ReadLine();
+		}
+
+		private static void ProcessInput()
+		{
+			while (true)
+			{
+				Console.Write("String to hash -> ");
+				var byteArray = Encoding.ASCII.GetBytes(Console.ReadLine());
+				var hash = _md5.Calculate(byteArray);
+				Console.WriteLine($"MD5 hash -> {hash}");
+			}
+		}
+
 		private static void Main(string[] arguments)
 		{
-			var md5 = new MD5();
-
 			if (arguments.Length > 0)
 			{
-				var validFiles = Utils.GetValidFiles(arguments);
-				var fileBytes = Utils.GetFilesBytes(validFiles);
-				var results = md5.Calculate(fileBytes);
-				for (var i = 0; i < results.Length; i++)
-				{
-					Console.WriteLine($"{validFiles[i]} -> {results[i]}");
-				}
+				ProcessFiles(arguments);
 			}
 			else
 			{
-				Console.WriteLine("String to hash -> ");
-				var byteArray = Encoding.ASCII.GetBytes(Console.ReadLine());
-				var hash = md5.Calculate(byteArray);
-				Console.WriteLine($"Hash -> {hash}");
+				ProcessInput();
 			}
 		}
 	}
